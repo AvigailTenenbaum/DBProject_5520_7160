ab58558ea10caafc42b94dddb8c527adb62edf42
AvigailTenenbaum <116683911+AvigailTenenbaum@users.noreply.github.com>
2024-05-26 15:53:19 +0300

    Delete שלב א directory

--- שלב א/bsnproj.sql
+++
@@ -1,7 +0,0 @@
-create user Avigail --user name
-IDENTIFIED BY aaa    --passward
-default tablespace SYSTEM
-temporary tablespace TEMP
-profile DEFAULT
-password expire;
-GRANT ALL PRIVILEGES TO Avigail
--- שלב א/dropTables.sql
+++
@@ -1,6 +0,0 @@
-drop table BORROWS;
-drop table BOOKSTOBORROW;
-drop table CUSTOMERS;
-drop table LIBRARIANS;
-drop table BOOKSCATALOG;
-drop table CATEGORIES;
